a0f40d4128ed82cf0938ce17a4de8ea657255ce1
Amira Deuraseh <amiradeuraseh@Amiras-MacBook-Pro.local>
2023-09-20 17:40:41 +0800

    👟 Module 4 Sole&Ankle

---
+++ sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import { WEIGHTS } from '../../constants';
+
+import Breadcrumbs from '../Breadcrumbs';
+import Select from '../Select';
+import Spacer from '../Spacer';
+import ShoeSidebar from '../ShoeSidebar';
+import ShoeGrid from '../ShoeGrid';
+
+const ShoeIndex = ({ sortId, setSortId }) => {
+  return (
+    <Wrapper>
+      <LeftColumn>
+        <Breadcrumbs>
+          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">
+            Shoes
+          </Breadcrumbs.Crumb>
+        </Breadcrumbs>
+        <Spacer size={42} />
+        <ShoeSidebar />
+      </LeftColumn>
+      <MainColumn>
+        <Header>
+          <Title>Running</Title>
+          <Select
+            label="Sort"
+            value={sortId}
+            onChange={(ev) => setSortId(ev.target.value)}
+          >
+            <option value="newest">Newest Releases</option>
+            <option value="price">Price</option>
+          </Select>
+        </Header>
+        <Spacer size={34} />
+        <ShoeGrid />
+      </MainColumn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 32px;
+`;
+
+const LeftColumn = styled.div``;
+
+const MainColumn = styled.div`
+  flex: 1;
+  margin-top: -8px;
+`;
+
+const Header = styled.header`
+  display: flex;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: ${WEIGHTS.medium};
+  margin-right: auto;
+`;
+
+export default ShoeIndex;
