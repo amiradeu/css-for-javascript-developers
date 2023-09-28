4786d31541c26747fe3ec28b271bc5b5f16ea9f5
amiradeu <amiradeu.x@gmail.com>
2023-09-28 15:21:52 +0800

    👟 [Better solution] Pt.2 Mobile Header
    Note: Seperating <MobileHeader> and <DesktopHeader> into it's own
    components seems like an easy way at first. But the more I
    study the solution, the more I believe how Josh did was much more
    better and cleaner.

--- 05-sole-and-ankle-revisited/src/components/Header/Header.js
+++ 05-sole-and-ankle-revisited/src/components/Header/Header.js
@@ -6,58 +6,51 @@ import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
 import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
-      <DesktopHeader>
         <SuperHeader />
         <MainHeader>
-          <Side>
-            <Logo />
-          </Side>
-          <Nav>
-            <NavLink href="/sale">Sale</NavLink>
-            <NavLink href="/new">New&nbsp;Releases</NavLink>
-            <NavLink href="/men">Men</NavLink>
-            <NavLink href="/women">Women</NavLink>
-            <NavLink href="/kids">Kids</NavLink>
-            <NavLink href="/collections">Collections</NavLink>
-          </Nav>
-          <Side />
+            <LogoWrapper>
+                <Logo />
+            </LogoWrapper>
+            <DesktopNav>
+                <NavLink href="/sale">Sale</NavLink>
+                <NavLink href="/new">New&nbsp;Releases</NavLink>
+                <NavLink href="/men">Men</NavLink>
+                <NavLink href="/women">Women</NavLink>
+                <NavLink href="/kids">Kids</NavLink>
+                <NavLink href="/collections">Collections</NavLink>
+            </DesktopNav>
+            <MobileActions>
+                <ShoppingBagButton>
+                    <Icon id="shopping-bag" />
+                    <VisuallyHidden>Open cart</VisuallyHidden>
+                </ShoppingBagButton>
+                <UnstyledButton>
+                    <Icon id="search" />
+                    <VisuallyHidden>Search</VisuallyHidden>
+                </UnstyledButton>
+                <UnstyledButton>
+                    <Icon id="menu" />
+                    <VisuallyHidden>Open menu</VisuallyHidden>
+                </UnstyledButton>
+            </MobileActions>
+            <Filler />
         </MainHeader>
-      </DesktopHeader>
 
-      <MobileHeader>
-        <MobileSuperHeader />
-        <MobileMainHeader>
-          <Logo />
-          <Side></Side>
-          <Icon id="shopping-bag" />
-          <Icon id="search" />
-          <Icon id="menu" onClick={() => setShowMobileMenu(true)} />
-        </MobileMainHeader>
         <MobileMenu
           isOpen={showMobileMenu}
           onDismiss={() => setShowMobileMenu(false)}
         />
-      </MobileHeader>
     </header>
   );
 };
-
-const DesktopHeader = styled.div`
-  @media ${QUERIES.tabletAndSmaller} {
-    display: none;
-  }
-`
 
 const MainHeader = styled.div`
   display: flex;
@@ -66,20 +59,47 @@ const MainHeader = styled.div`
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
 
+  @media ${QUERIES.tabletAndSmaller} {
+    border-top: 4px solid ${COLORS.gray[900]};
+    justify-content: space-between;
+    align-items: center;
+  }
+
   @media ${QUERIES.phoneAndSmaller} {
     padding-inline: 16px;
   }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
-const Side = styled.div`
-  flex: 1;
-`;
+const MobileActions = styled.div`
+    display: none;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: flex;
+        gap: 32px;
+    }
+
+    @media ${QUERIES.phoneAndSmaller} {
+      gap: 16px;
+    }
+`
+
+const LogoWrapper = styled.div`
+    flex: 1;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        flex: revert;
+    }
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -93,24 +113,17 @@ const NavLink = styled.a`
   }
 `;
 
-const MobileHeader = styled.div`
-  display: none;
+// custom modification to ensure evenly space icons
+const ShoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
+`
+
+const Filler = styled.div`
+  flex: 1;
 
   @media ${QUERIES.tabletAndSmaller} {
-    display: block;
+    display: none;
   }
-`
+`;
 
-const MobileSuperHeader = styled(MobileHeader)`
-    height: 4px;
-    background-color: ${COLORS.gray[900]}
-`
-
-const MobileMainHeader = styled(MainHeader)`
-  gap: 32px;
-
-  @media ${QUERIES.phoneAndSmaller} {
-    gap: 16px;
-  }
-`
 export default Header;
