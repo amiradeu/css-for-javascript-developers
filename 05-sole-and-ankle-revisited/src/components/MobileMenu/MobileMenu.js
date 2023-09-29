30f7dad20bd7b62ea55aec941425ec982dec0176
amiradeu <amiradeu.x@gmail.com>
2023-09-29 12:48:55 +0800

    👟 Pt.4 Mobile Menu [My Solution]

--- 05-sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
+++ 05-sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
@@ -6,6 +6,7 @@ import { DialogOverlay, DialogContent } from '@reach/dialog';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import { COLORS, WEIGHTS } from '../../constants';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -13,23 +14,85 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content aria-label="Mobile Navigation">
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
 
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: hsl(220deg 5% 40% / 0.8);
+  display: flex;
+  flex-direction: row-reverse;
+`
+
+const Content = styled(DialogContent)`
+  position: relative;
+  /* width: 300px; */
+  padding: 32px;
+  background: ${COLORS.white};
+  display: flex;
+  flex: 0 1 300px;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const CloseButton = styled(UnstyledButton)`
+  margin-left: auto;
+`
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`
+
+const Link = styled.a`
+  text-decoration: none;
+`
+
+const NavLink = styled(Link)`
+  text-transform: uppercase;
+  color: ${COLORS.gray[900]};
+  font-size: ${18 / 16}rem;
+  font-weight: ${WEIGHTS.medium};
+
+  &:hover {
+    color: ${COLORS.secondary};
+  }
+`
+
+const FooterLink = styled(Link)`
+  color: ${COLORS.gray[700]};
+  font-size: ${14 / 16}rem;
+`
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`
 export default MobileMenu;
