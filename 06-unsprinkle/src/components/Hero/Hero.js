44cf3bb3747954b1eb16d67b842b31ab8ff67721
amiradeu <amiradeu.x@gmail.com>
2023-10-05 11:15:02 +0800

    🖼️  Update Unsprinkle with Responsive Images

--- 06-unsprinkle/src/components/Hero/Hero.js
+++ 06-unsprinkle/src/components/Hero/Hero.js
@@ -4,8 +4,29 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+          /images/hero-img.avif 1x,
+          /images/hero-img@2x.avif 2x,
+          /images/hero-img@3x.avif 3x
+          "
+        />
+        <source
+          type="image/jpg"
+          srcSet="
+          /images/hero-img.jpg 1x,
+          /images/hero-img@2x.jpg 2x,
+          /images/hero-img@3x.jpg 3x
+          "
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="Unsprinkle featured image"
+        />
+      </picture>
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
@@ -23,8 +44,10 @@ const Wrapper = styled.section`
 const HeroImage = styled.img`
   display: block;
   width: 500px;
+  max-width: 100%;
   height: 500px;
   max-height: 100%;
+  object-fit: cover;
 `;
 
 const Swoop = styled.img`
