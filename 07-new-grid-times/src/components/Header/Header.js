934f0a2c9e4712b4c15af961258784a03a2311e9
amiradeu <amiradeu.x@gmail.com>
2023-10-21 14:10:48 +0800

    Ex1: Header

--- 07-new-grid-times/src/components/Header/Header.js
+++ 07-new-grid-times/src/components/Header/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
+import VisuallyHidden from '../VisuallyHidden';
 import Logo from '../Logo';
 import Button from '../Button';
 
@@ -29,7 +30,24 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActions>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopActions>
         <Logo />
+        <SubscribeWrapper>
+          <Button>
+            Subscribe
+          </Button>
+          <SubscribeCaption href="#">Already a subscriber?</SubscribeCaption>
+          <VisuallyHidden>Subscribe</VisuallyHidden>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +57,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +87,35 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+  }
 `;
 
+const DesktopActions = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+  }
+`
+
+const SubscribeCaption = styled.a`
+  font-style: italic;
+  text-decoration: underline;
+  font-size: ${14/16}rem;
+  color: ${COLORS.gray[900]};
+`
 export default Header;
