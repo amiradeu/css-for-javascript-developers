56ab398aab36aa05221d6e034a2e68ce95c47c61
amiradeu <amiradeu.x@gmail.com>
2023-10-23 13:19:11 +0800

    📰 Ex4 Improved solution

--- 07-new-grid-times/src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ 07-new-grid-times/src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -38,7 +38,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+            <SportsStoryWrapper key={data.id}>
+              <MiniStory {...data} />
+            </SportsStoryWrapper>
           ))}
         </SportsStories>
       </SportsSection>
@@ -50,40 +52,54 @@ const Wrapper = styled.div`
   display: grid;
   gap: 48px;
 
+  @media ${QUERIES.tabletAndUp} {
+    gap: 64px;
+    grid-template-columns: 100%;
+    /* alternative: allow to shrink below auto fr size */
+    /* grid-template-columns: minmax(0px, auto); */
+  }
+
   @media ${QUERIES.laptopAndUp} {
     gap: 0px;
     grid-template-columns: 50% 50%;
+    /* alternative: allow to shrink below auto fr size */
+    /* grid-template-columns: 1fr minmax(0px, 1fr); */
   }
 `;
 
 const MarketsSection = styled.section`
   @media ${QUERIES.laptopAndUp} {
-    padding-inline-end: 16px;
+    padding-right: 16px;
+    margin-right: 16px;
     border-right: 1px solid var(--color-gray-300);
   }
 `;
 
 const MarketCards = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(184px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(165px, 1fr));
   gap: 16px;
 `;
 
 const SportsSection = styled.section`
-  @media ${QUERIES.laptopAndUp} {
-      padding-inline-start: 16px;
-  }
 `;
 
 const SportsStories = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(184px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(165px, 1fr));
   gap: 16px;
 
   @media ${QUERIES.tabletAndUp} {
     display: flex;
+    grid-template-columns: revert;
     overflow: auto;
   }
 `;
+
+const SportsStoryWrapper = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    min-width: 220px;
+  }
+`
 
 export default SpecialtyStoryGrid;
